33c4b7bfc5dd3ceec8b7bd91cc0b3feb003c88a6
Gabriel Malaluan <gabriel_malaluan@yahoo.com>
2021-09-16 11:02:33 +0800

    Modifying button primary color and alignments

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import './Content.css';
 import graph from '../assets/Assets/Hero_Image_(Desktop_View).png';
 import twitter from '../assets/Assets/Twitter.svg';
@@ -10,42 +8,45 @@ const Content = () => {
   let inlineAvatarsPos = 0;
 
   return (
-    <div className="Content-body bg-cream">
-      <div className="container-md py-5">
+    <div className="Content-body bg-cream py-5">
+      <div className="container-md">
         <div className="row">
           <div className="col">
-            <h1 className="display-3">
-              <b>Twitter analytics taken to a whole new level.</b>
-            </h1>
-            <p>
-              Chirp is a suite of Twitter analytics that will help you better
-              undestand your audience, whice tweets they like, and most
-              importantly, when they are the msot active Twitter.
-            </p>
-            <div>
-              <button className="btn btn-primary">
-                <img src={twitter} className="me-2" alt="twitter" />
-                Sign in with Twitter
-              </button>
-              <a href="#">Learn more</a>
-            </div>
-            <div className="inline-avatars">
-              {userAvatars.map(
-                (val, index) => (
-                  (inlineAvatarsPos -= 20),
-                  (
-                    <img
-                      src={val}
-                      alt={val}
-                      style={
-                        index > 0
-                          ? { left: inlineAvatarsPos }
-                          : { position: 'relative' }
-                      }
-                    />
-                  )
-                )
-              )}
+            <div className="sec1-col1">
+              <h1 className="display-3">
+                <strong>Twitter analytics taken to a whole new level.</strong>
+              </h1>
+              <p>
+                Chirp is a suite of Twitter analytics that will help you better
+                undestand your audience, whice tweets they like, and most
+                importantly, when they are the msot active Twitter.
+              </p>
+              <div>
+                <button className="btn btn-primary">
+                  <img src={twitter} className="me-2" alt="twitter" />
+                  Sign in with Twitter
+                </button>
+                <a href="#">Learn more</a>
+              </div>
+              <div className="inline-avatars">
+                {userAvatars.map((val, index) => (
+                  <img
+                    src={val}
+                    key={index}
+                    alt={val}
+                    style={
+                      index > 0
+                        ? { left: (inlineAvatarsPos -= 20) }
+                        : { position: 'relative' }
+                    }
+                  />
+                ))}
+              </div>
+              <p>
+                Join <strong className="text-darkteal">195</strong> others who
+                have analyzed their followers and scheduled{' '}
+                <strong className="text-darkteal">1342</strong> tweets!
+              </p>
             </div>
           </div>
           <div className="col d-flex justify-content-end">
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Header.css';
 import twitter from '../assets/Assets/Twitter.svg';
 
@@ -7,13 +6,17 @@ const Header = () => {
   return (
     <div className="bg-cream">
       <div className="container-md py-3 d-flex justify-content-between align-items-center">
-        <div className="bd-highlight">Chirp</div>
+        <div className="bd-highlight text-primary">
+          <h4>
+            <i>Chirp.</i>
+          </h4>
+        </div>
         <div className="d-flex align-items-center">
-          <a href="#">Home</a>
-          <a href="#" className="mx-3">
+          <a href="/#">Home</a>
+          <a href="/#" className="mx-5">
             Pricing
           </a>
-          <a href="#" className="me-3">
+          <a href="/#" className="me-5">
             FAQ
           </a>
           <button className="btn btn-primary">
